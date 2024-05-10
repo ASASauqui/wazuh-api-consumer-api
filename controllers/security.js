31fcb2ce6a95f0553c35edae782e494aa98f4890
Alan Salazar <alansalazar@jankowski.li>
2024-05-09 22:02:01 -0600

    More security endpoints were implemented, related to users

--- controllers/security.js
+++ controllers/security.js
@@ -51,7 +51,89 @@ const userLogout = async (req, res) => {
     }
 };
 
+const listUsers = async (req, res) => {
+    try {
+        const authorization = req.headers.authorization;
+
+        if (!authorization) {
+            return res.status(400).json({ message: 'Token is required' });
+        }
+
+        const response = await axios.get(`${process.env.API_URL}/security/users`, {
+            headers: { 'Authorization': authorization },
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false
+            })
+        });
+
+        return res.status(response.status).json(response.data);
+    }
+    catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+const addUser = async (req, res) => {
+    try {
+        const authorization = req.headers.authorization;
+
+        if (!authorization) {
+            return res.status(400).json({ message: 'Token is required' });
+        }
+
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            return res.status(400).json({ message: 'Username and password are required' });
+        }
+
+        const response = await axios.post(`${process.env.API_URL}/security/users`, { username, password }, {
+            headers: { 'Authorization': authorization },
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false
+            })
+        });
+
+        return res.status(response.status).json(response.data);
+    }
+    catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+const deleteUsers = async (req, res) => {
+    try {
+        const authorization = req.headers.authorization;
+
+        if (!authorization) {
+            return res.status(400).json({ message: 'Token is required' });
+        }
+
+        const { user_ids } = req.query;
+
+        if (!user_ids) {
+            return res.status(400).json({ message: 'User IDs are required' });
+        }
+
+        const response = await axios.delete(`${process.env.API_URL}/security/users?user_ids=${user_ids}`, {
+            headers: { 'Authorization': authorization },
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false
+            })
+        });
+
+        return res.status(response.status).json(response.data);
+    }
+    catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+
 module.exports = {
     userAuthenticate,
-    userLogout
+    userLogout,
+    listUsers,
+    addUser,
+    deleteUsers
 };
--- routes/security.js
+++ routes/security.js
@@ -4,10 +4,16 @@ const router = express.Router();
 
 const {
     userAuthenticate,
-    userLogout
+    userLogout,
+    listUsers,
+    addUser,
+    deleteUsers
 } = require('../controllers/security');
 
 router.post('/user/authenticate', userAuthenticate);
 router.delete('/user/authenticate', userLogout);
+router.get('/users', listUsers);
+router.post('/users', addUser);
+router.delete('/users', deleteUsers);
 
 module.exports = router;
